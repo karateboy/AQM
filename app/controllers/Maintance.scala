23abb1429e03dfce46a11d02e41ab756ed43948f
Aragorn Huang <karateboy.tw@gmail.com>
2015-09-29 00:46:23 +0800

    More form

--- app/controllers/Maintance.scala
+++ app/controllers/Maintance.scala
@@ -19,18 +19,6 @@ import play.api.libs.functional.syntax._
  * @author user
  */
 object Maintance extends Controller {
-  def myCase = Security.Authenticated {
-    implicit request =>
-      val userInfoOpt = Security.getUserinfo(request)
-      if (userInfoOpt.isEmpty) {
-        Forbidden("Invalid access!")
-      } else {
-        val userInfo = userInfoOpt.get
-        val group = Group.getGroup(userInfo.groupID).get
-        Ok("")
-      }
-  }
-
   def newTicket = Security.Authenticated {
     implicit request =>
       val userInfo = Security.getUserinfo(request).get
@@ -40,15 +28,15 @@ object Maintance extends Controller {
       Ok(views.html.newTicket(userInfo, group.privilege, adminUsers))
   }
 
-  case class NewTicketParam(ticketType: TicketType.Value, monitors: Seq[Monitor.Value],
-                            monitorTypes: Seq[MonitorType.Value], reason: String, owner:Int, executeDate: Seq[String])
+  case class TicketParam(ticketType: TicketType.Value, monitors: Seq[Monitor.Value],
+                         monitorTypes: Seq[MonitorType.Value], reason: String, owner: Int, executeDate: Seq[String])
 
-  implicit val newTicketParamRead = Json.reads[NewTicketParam]
+  implicit val newTicketParamRead = Json.reads[TicketParam]
 
   def newTicketAction(idStr: String) = Security.Authenticated(BodyParsers.parse.json) {
     implicit request =>
       val submiterId = Integer.parseInt(idStr)
-      val newTicketJson = request.body.validate[NewTicketParam]
+      val newTicketJson = request.body.validate[TicketParam]
 
       newTicketJson.fold(
         error => {
@@ -63,7 +51,7 @@ object Maintance extends Controller {
                 date <- ticketParam.executeDate
               } yield {
                 Ticket(0, DateTime.now, true, ticketParam.ticketType, submiterId,
-                  ticketParam.owner, m, None, ticketParam.reason, DateTime.parse(date))
+                  ticketParam.owner, m, None, ticketParam.reason, DateTime.parse(date), Ticket.defaultFormData)
               }
             } else {
               for {
@@ -72,16 +60,15 @@ object Maintance extends Controller {
                 date <- ticketParam.executeDate
               } yield {
                 Ticket(0, DateTime.now, true, ticketParam.ticketType, submiterId,
-                  ticketParam.owner, m, Some(mt), ticketParam.reason, DateTime.parse(date))
+                  ticketParam.owner, m, Some(mt), ticketParam.reason, DateTime.parse(date), Ticket.defaultFormData)
               }
             }
           Logger.debug("# of ticket=" + tickets.length)
           for (t <- tickets)
             Ticket.newTicket(t)
 
-          Ok(Json.obj("ok" -> true, "nNewCase"->tickets.length))
+          Ok(Json.obj("ok" -> true, "nNewCase" -> tickets.length))
         })
-
   }
 
   def queryTicket = Security.Authenticated {
@@ -92,23 +79,131 @@ object Maintance extends Controller {
 
       Ok(views.html.queryTicket(userInfo, group.privilege, adminUsers))
   }
-  
-  def ticketReport(ticketTypeStr:String, monitorStr:String, startStr:String, endStr:String) = Security.Authenticated {
+
+  def ticketReport(ticketTypeStr: String, monitorStr: String, startStr: String, endStr: String) = Security.Authenticated {
     val ticketTypes = ticketTypeStr.split(":").map { TicketType.withName }
     val monitors = monitorStr.split(":").map { Monitor.withName }
     val start = DateTime.parse(startStr)
     val end = DateTime.parse(endStr) + 1.day
-    
+
     val tickets = Ticket.queryTickets(start, end)
     val filterTicket = tickets.filter { t => ticketTypes.contains(t.ticketType) && monitors.contains(t.monitor) }
     val adminUsers = User.getAdminUsers()
-    val usrMap = Map(adminUsers.map{u=>(u.id.get->u)} :_*)
-    
+    val usrMap = Map(adminUsers.map { u => (u.id.get -> u) }: _*)
+
     Ok(views.html.ticketReport(filterTicket, usrMap))
   }
+
+  def myTicket = Security.Authenticated {
+    implicit request =>
+      val userInfoOpt = Security.getUserinfo(request)
+      if (userInfoOpt.isEmpty) {
+        Forbidden("Invalid access!")
+      } else {
+        val userInfo = userInfoOpt.get
+        val group = Group.getGroup(userInfo.groupID).get
+        val tickets = Ticket.myTickets(userInfo.id)
+        val adminUsers = User.getAdminUsers()
+        val usrMap = Map(adminUsers.map { u => (u.id.get -> u) }: _*)
+
+        Ok(views.html.ticketReport(tickets, usrMap))
+      }
+  }
+
+  def ticket(ID: Int) = Security.Authenticated {
+    implicit request =>
+
+      val ticketOpt = Ticket.getTicket(ID)
+
+      if (ticketOpt.isEmpty)
+        BadRequest("No such ticket!")
+      else {
+        val userInfo = Security.getUserinfo(request).get
+        val group = Group.getGroup(userInfo.groupID).get
+        val adminUsers = User.getAdminUsers()
+
+        Ok(views.html.ticket(ticketOpt.get, group.privilege, adminUsers))
+      }
+  }
+
+  def updateTicket(ID: Int) = Security.Authenticated(BodyParsers.parse.json) {
+    implicit request =>
+      val ticketResult = request.body.validate[TicketParam]
+
+      ticketResult.fold(
+        error => {
+          Logger.error(JsError.toFlatJson(error).toString())
+          BadRequest(Json.obj("ok" -> false, "msg" -> JsError.toFlatJson(error)))
+        },
+        param => {
+          val ticketOpt = Ticket.getTicket(ID)
+          if (ticketOpt.isEmpty)
+            BadRequest(Json.obj("ok" -> false, "msg" -> "無此案件"))
+          else {
+            val t = ticketOpt.get
+            val mt = if (param.monitorTypes.length == 0)
+              None
+            else
+              Some(param.monitorTypes(0))
+            val executeDate = DateTime.parse(param.executeDate(0))
+            val newT = Ticket(ID,
+              t.submit_date,
+              t.active,
+              param.ticketType,
+              t.submiter_id,
+              param.owner,
+              param.monitors(0),
+              mt,
+              param.reason,
+              executeDate, t.form)
+
+            Ticket.updateTicket(newT)
+            Ok(Json.obj("ok" -> true))
+          }
+        })
+  }
+
+  def updateForm(ID: Int) = Security.Authenticated(BodyParsers.parse.json) {
+    import Ticket._
+    implicit request =>
+      val formResult = request.body.validate[FormData]
+
+      formResult.fold(
+        error => {
+          Logger.error(JsError.toFlatJson(error).toString())
+          BadRequest(Json.obj("ok" -> false, "msg" -> JsError.toFlatJson(error)))
+        },
+        form => {
+          Logger.debug("bool #=>"+ form.boolValues.length)
+          Logger.debug("str #=>"+ form.strValues.length)
+          Logger.debug("str #=>"+ form.comments.length)
+          
+          Ticket.updateTicketFormData(ID, form)
+          Ok(Json.obj("ok" -> true))
+        })
+  }
   
-  def updateTicket = Security.Authenticated {
-    Ok("")
+  def getForm(ID: Int) = Security.Authenticated {
+    val ticketOpt = Ticket.getTicket(ID)
+    if (ticketOpt.isEmpty)
+      BadRequest(Json.obj("ok" -> false, "msg" -> "無此案件"))
+    else {
+      val t = ticketOpt.get
+      t.ticketType match {
+          case TicketType.maintance_week=>
+            Ok(views.html.weekMaintanceForm(ID, t.form))
+          case TicketType.maintance_biweek=>
+            Ok(views.html.biweekForm(ID, t.form))
+          case TicketType.maintance_month=>
+            Ok(views.html.monthForm(ID, t.form))
+          case TicketType.maintance_quarter=>
+            Ok(views.html.quarterForm(ID, t.form))
+          case TicketType.maintance_half_year=>
+            Ok(views.html.halfYearForm(ID, t.form))
+          case TicketType.maintance_year=>
+            Ok(views.html.yearForm(ID, t.form))
+      }
+    }
   }
 
   def closeTicket = Security.Authenticated {
--- app/models/Ticket.scala
+++ app/models/Ticket.scala
@@ -8,51 +8,96 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Json
 
-object TicketType extends Enumeration{
+case class FormData(start: String, end: String, boolValues: Seq[Boolean], strValues: Seq[String], comments: Seq[String]) {
+  def getBool(idx: Int) = {
+    if (idx >= boolValues.length)
+      false
+    else
+      boolValues(idx)
+  }
+
+  var idx = 0
+  def getBoolSeq() = {
+    val ret = getBool(idx)
+    idx += 1
+    if(ret)
+      "checked"
+    else
+      ""
+  }
+
+  var strIdx = 0
+  def getStr(idx: Int) = {
+    if (idx >= strValues.length)
+      ""
+    else
+      strValues(idx)
+  }
+
+  def getStrSeq() = {
+    val ret = getStr(strIdx)
+    strIdx += 1
+    ret
+  }
+  
+  def getComment(idx:Int) = {
+    if(idx >= comments.length)
+      ""
+    else
+      comments(idx)
+  }
+}
+
+object TicketType extends Enumeration {
   val maintance_week = Value
   val maintance_biweek = Value
   val maintance_month = Value
   val maintance_quarter = Value
+  val maintance_half_year = Value
   val maintance_year = Value
   val repair = Value
 
-  val map = Map(repair->"維修", maintance_week->"單周定保", maintance_biweek->"雙周定保", 
-      maintance_month->"月定保", maintance_quarter->"季定保", maintance_year->"年定保")
-      
-  def withId(id:Int)={
-    if(id == repair.id)
+  val map = Map(repair -> "維修", maintance_week -> "單周定保", maintance_biweek -> "雙周定保",
+    maintance_month -> "月定保", maintance_quarter -> "季定保", maintance_half_year->"半年定保", maintance_year -> "年定保")
+
+  def withId(id: Int) = {
+    if (id == repair.id)
       repair
-    else if(id == maintance_week.id)
+    else if (id == maintance_week.id)
       maintance_week
-    else if(id == maintance_biweek.id)
+    else if (id == maintance_biweek.id)
       maintance_biweek
-    else if(id == maintance_month.id)
+    else if (id == maintance_month.id)
       maintance_month
-    else if(id == maintance_quarter.id)
+    else if (id == maintance_quarter.id)
       maintance_quarter
-    else if(id == maintance_year.id)
+    else if (id == maintance_half_year.id)
+      maintance_half_year
+    else if (id == maintance_year.id)
       maintance_year
     else
       throw new NoSuchElementException
   }
-  
+
   implicit val tReads: Reads[TicketType.Value] = EnumUtils.enumReads(TicketType)
   implicit val tWrites: Writes[TicketType.Value] = EnumUtils.enumWrites
 }
 
-case class Ticket(id:Int, submit_date:DateTime, active:Boolean, ticketType:TicketType.Value, submiter_id:Int, owner_id:Int, monitor:Monitor.Value, 
-    monitorType:Option[MonitorType.Value], reason:String, executeDate:DateTime)
+case class Ticket(id: Int, submit_date: DateTime, active: Boolean, ticketType: TicketType.Value, submiter_id: Int, owner_id: Int, monitor: Monitor.Value,
+                  monitorType: Option[MonitorType.Value], reason: String, executeDate: DateTime, form: FormData)
 /**
  * @author user
  */
 object Ticket {
   import scala.collection.mutable.ArrayBuffer
+  implicit val formDataRead = Json.reads[FormData]
+  implicit val formDataWrite = Json.writes[FormData]
 
   def newTicket(ticket: Ticket)(implicit session: DBSession = AutoSession) = {
     DB localTx { implicit session =>
-      val submit_tt:java.sql.Timestamp = ticket.submit_date
-      val execute_date:java.sql.Date = ticket.executeDate
-      
+      val submit_tt: java.sql.Timestamp = ticket.submit_date
+      val execute_date: java.sql.Date = ticket.executeDate
+
       sql"""
         Insert into Ticket(
         [submit_date]
@@ -63,45 +108,113 @@ object Ticket {
         ,[monitor]
         ,[monitorType]
         ,[reason]
-        ,[execute_date])
+        ,[execute_date]
+        ,[form])
         values(${submit_tt}, ${ticket.active}, ${ticket.ticketType.id}, ${ticket.submiter_id}, ${ticket.owner_id}, ${ticket.monitor.toString},
-          ${ticket.monitorType}, ${ticket.reason}, ${execute_date})
+          ${ticket.monitorType.map { _.toString }}, ${ticket.reason}, ${execute_date}, ${Json.toJson(defaultFormData).toString})
         """.update.apply
     }
   }
-  
-  def allTickets(implicit session: DBSession = AutoSession) = {
+
+  def queryTickets(start: DateTime, end: DateTime)(implicit session: DBSession = AutoSession) = {
     sql"""
-      Select *
-      From Ticket
-      Order by submit_date      
-      """.map { r =>
-      Ticket(r.int(1), r.timestamp(2), r.boolean(3), TicketType.withId(r.int(4)), 
-          r.int(5), r.int(6), Monitor.withName(r.string(7)), 
-          if(r.stringOpt(8).isEmpty)
-            None
-          else
-            Some(MonitorType.withName(r.string(8))),
-          r.string(9), r.date(10))
-    }.list().apply()
-  }
-  
-  def queryTickets(start:DateTime, end:DateTime)(implicit session: DBSession = AutoSession) = {
-        sql"""
       Select *
       From Ticket
       Where submit_date between ${start} and ${end}
       Order by submit_date      
       """.map { r =>
-      Ticket(r.int(1), r.timestamp(2), r.boolean(3), TicketType.withId(r.int(4)), 
-          r.int(5), r.int(6), Monitor.withName(r.string(7)), 
-          if(r.stringOpt(8).isEmpty)
-            None
-          else
-            Some(MonitorType.withName(r.string(8))),
-          r.string(9), r.date(10))
+      Ticket(r.int(1), r.timestamp(2), r.boolean(3), TicketType.withId(r.int(4)),
+        r.int(5), r.int(6), Monitor.withName(r.string(7)),
+        if (r.stringOpt(8).isEmpty)
+          None
+        else
+          Some(MonitorType.withName(r.string(8))),
+        r.string(9), r.date(10),
+        if (r.stringOpt(11).isEmpty)
+          defaultFormData
+        else
+          Json.parse(r.string(11)).validate[FormData].get)
     }.list().apply()
-
   }
-  
+
+  def myTickets(ID: Int)(implicit session: DBSession = AutoSession) = {
+    sql"""
+      Select *
+      From Ticket
+      Where owner_id = ${ID}
+      Order by submit_date      
+      """.map { r =>
+      Ticket(r.int(1), r.timestamp(2), r.boolean(3), TicketType.withId(r.int(4)),
+        r.int(5), r.int(6), Monitor.withName(r.string(7)),
+        if (r.stringOpt(8).isEmpty)
+          None
+        else
+          Some(MonitorType.withName(r.string(8))),
+        r.string(9), r.date(10),
+        if (r.stringOpt(11).isEmpty)
+          defaultFormData
+        else
+          Json.parse(r.string(11)).validate[FormData].get)
+    }.list().apply()
+  }
+
+  def getTicket(ID: Int)(implicit session: DBSession = AutoSession) = {
+    sql"""
+      Select *
+      From Ticket
+      Where id = ${ID}
+      """.map { r =>
+      Ticket(r.int(1), r.timestamp(2), r.boolean(3), TicketType.withId(r.int(4)),
+        r.int(5), r.int(6), Monitor.withName(r.string(7)),
+        if (r.stringOpt(8).isEmpty)
+          None
+        else
+          Some(MonitorType.withName(r.string(8))),
+        r.string(9), r.date(10),
+        if (r.stringOpt(11).isEmpty)
+          defaultFormData
+        else
+          Json.parse(r.string(11)).validate[FormData].get)
+    }.single().apply()
+  }
+
+  def updateTicket(ticket: Ticket)(implicit session: DBSession = AutoSession) = {
+    DB localTx { implicit session =>
+      sql"""
+        Update Ticket
+        Set [ticketType]=${ticket.ticketType.id}, [owner_id]=${ticket.owner_id},
+          [monitor]=${ticket.monitor.toString}, [monitorType]=${ticket.monitorType.map { _.toString }},
+          [reason]=${ticket.reason}, [execute_date]=${ticket.executeDate.toDate}
+        Where ID = ${ticket.id}
+        """.update.apply
+    }
+  }
+
+  val defaultFormData = FormData("", "", Seq.empty[Boolean], Seq.empty[String], Seq.empty[String])
+
+  def getTicketForm(ID: Int)(implicit session: DBSession = AutoSession) = {
+    sql"""
+      Select *
+      From Ticket
+      Where id = ${ID}
+      """.map { r =>
+      val formJsonOpt = r.stringOpt(11)
+      if (formJsonOpt.isEmpty)
+        defaultFormData
+      else {
+        val formJson = formJsonOpt.get
+        Json.parse(formJson).validate[FormData].get
+      }
+    }.single().apply()
+  }
+
+  def updateTicketFormData(ID: Int, form: FormData)(implicit session: DBSession = AutoSession) = {
+    DB localTx { implicit session =>
+      sql"""
+        Update Ticket
+        Set [form]=${Json.toJson(form).toString}
+        Where ID = ${ID}
+        """.update.apply
+    }
+  }
 }
